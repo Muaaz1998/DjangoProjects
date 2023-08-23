68d35b78369265196eab8120be315a7d1904831f
Muaaz1998 <muaaz@LAPTOP-CB7P4BF3.localdomain>
2023-08-24 01:14:17 +1000

    Added password reset functionality using gmail SMTP

--- news/users/urls.py
+++ news/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, reverse_lazy
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 from .views import SignupView, UserLoginView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name = "signup"),
-    path("login/", UserLoginView.as_view(), name = "login")
+    path("login/", UserLoginView.as_view(), name = "login"),
+    path("reset_password/", auth_views.PasswordResetView.as_view(), name = "password reset")
 
 ]
